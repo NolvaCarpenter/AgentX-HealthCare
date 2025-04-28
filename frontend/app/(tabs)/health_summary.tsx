b039b1eb83e01cd9ac25577bded4261fc474033f
Nolva <glaceon0214@gmail.com>
2025-04-29 00:33:18 +0900

    Modall

--- frontend/app/(tabs)/health_summary.tsx
+++ frontend/app/(tabs)/health_summary.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
+import ModalManager from '@/components/ModalManager'
 import React from 'react'
 
 export default function health_summary() {
   return (
     <View>
-      <Text>health_summary</Text>
+      <ModalManager/>
     </View>
   )
 }
